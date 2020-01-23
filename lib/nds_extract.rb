eec622e98bd7b1d6db19775b23f5406969c41110
Lee McCormick <lzm0010@gmail.com>
2020-01-23 01:53:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,7 +17,7 @@ def directors_totals(nds)
   
   director_index = 0
   while director_index < nds.length do
-    director = nds[director_index]
+    director = nds[director_index][:name]
     
     director_index += 1
   end
